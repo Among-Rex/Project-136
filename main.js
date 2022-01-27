a47e942f2cd486203b725bb3670bcb0ee5708efb
Among-Rex <80576723+Among-Rex@users.noreply.github.com>
2022-01-27 17:38:49 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+status = "";
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}

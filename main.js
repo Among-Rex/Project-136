a3d0276f5b8816440ba254d78b22a6c6a06cf5b8
Among-Rex <80576723+Among-Rex@users.noreply.github.com>
2022-01-28 14:29:23 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+video = "";
 status = "";
+
+function preload() {
+    video = createVideo('video.mp4');
+    video.hide();
+}
 
 function setup() {
     canvas = createCanvas(480, 380);
     canvas.center();
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
 }
 
 function start() {
@@ -13,4 +23,7 @@ function start() {
 function modelLoaded() {
     console.log("Model Loaded!");
     status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
 }
